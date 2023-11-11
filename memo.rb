ffe7ea73226781f25d0a2b28532c22b31150001a
UkiFukui <fukuiuki@gmail.com>
2023-11-11 23:42:48 +0900

    提出1回目

--- memo.rb
+++ memo.rb
@@ -1,26 +1,55 @@
-# ruby memo.rb
-
-require "csv" # CSVファイルを扱うためのライブラリを読み込んでいます
+require "csv"
 
 puts "1 → 新規でメモを作成する / 2 → 既存のメモを編集する"
 puts "（数字は半角で入力してください）"
 
 memo_type = gets.to_i # ユーザーの入力値を取得し、数字へ変換しています
 
-# if文を使用して続きを作成していきましょう。
-# 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう。
+# 「memo_type」の値（1 or 2）によって処理を分岐させるメソッド
 def seletct_new_old(memo_type)
   if memo_type == 1
-    puts "1が入力されました" ##デバッグ
+    edit_new_file
   elsif memo_type == 2
-    puts "2が入力されました" ##デバッグ
+    edit_old_file
   else
-    until [1, 2].include?(memo_type)
+    until [1, 2].include?(memo_type) # 1,2が入力されるまで処理を繰り返す
       puts "半角で1か2を入力してください"
       memo_type = gets.to_i
     end
     seletct_new_old(memo_type)
   end
+end
+
+# 新規ファイルを作成し書き込みを行うメソッド
+def edit_new_file
+  puts "作成したいファイル名（拡張子は除く）を入力してください"
+    file_name = gets.to_s.chomp!
+  puts "メモしたい内容を記入してください"
+  puts "完了したら、最後に改行をしてからCtrl + Dを押してください"
+    text = []
+    while line = gets
+      text << line.chomp
+    end
+    CSV.open("#{file_name}.csv","w") do |csv|
+      csv << text
+    end
+  puts "新規ファイル[#{file_name}.csv]を作成しました"
+end
+
+# 既存ファイルに追記するメソッド
+def edit_old_file
+  puts "編集したいファイル名（拡張子は除く）を入力してください"
+    file_name = gets.to_s.chomp!
+  puts "メモしたい内容を記入してください"
+  puts "完了したらCtrl + Dを押してください"
+    text = []
+    while line = gets
+      text << line.chomp
+    end
+    CSV.open("#{file_name}.csv","a") do |csv|
+      csv << text
+  end
+puts "入力内容を[#{file_name}.csv]に追記しました"
 end
 
 seletct_new_old(memo_type)
